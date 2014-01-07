98ed005c1152d14ce5b1203405427e53dbd9d161
vzvenyach <vzvenyach@dccouncil.us>
2014-01-07 10:45:30 -0500

    first commit

---
+++ app.js
@@ -0,0 +1,61 @@
+
+/**
+ * Module dependencies.
+ */
+
+var express = require('express');
+var routes = require('./routes');
+var user = require('./routes/user');
+var http = require('http');
+var path = require('path');
+
+var app = express();
+var cons = require('consolidate');
+var redis = require('redis');
+
+// all environments
+app.set('port', process.env.PORT || 5000);
+app.set('views', path.join(__dirname, 'views'));
+//app.set('view engine', 'jade');
+//app.use(express.favicon());
+app.use(express.logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded());
+app.use(express.methodOverride());
+app.use(app.router);
+app.use(express.static(path.join(__dirname, 'public')));
+
+// Registering underscore template
+app.engine('html', cons.underscore);
+app.set('view engine', 'html');
+
+//create redis client
+var client = redis.createClient();
+
+// development only
+if ('development' == app.get('env')) {
+  app.use(express.errorHandler());
+}
+
+app.get('/', routes.index);
+app.get('/users', user.list);
+
+
+app.get("/counts.json", function	(req, res) {
+    client.get("motionstest", function	(error, motionstest) {
+	if (error !== null) {
+            // handle error here                                                                                                                       
+            console.log("ERROR: " + error);
+        } else {
+            var jsonObject = {
+		"motionstest":motionstest
+            };
+            // use res.json to return JSON objects instead of strings
+            res.json(jsonObject);
+        }
+    });
+});
+
+http.createServer(app).listen(app.get('port'), function(){
+  console.log('Express server listening on port ' + app.get('port'));
+});
---
+++ readme.md
@@ -0,0 +1,80 @@
+# readme.md
+
+# What this is about?
+
+# A discussion about motions types
+
+Motions can come in three main flavors (called "classes"):
+
+1. ***Main motion***. The main motion is the measure to be voted on (e.g., Bill 20-1111).
+2. ***Subsidiary motions***.
+3. ***Incidental motions***.
+
+There are also ***privileged motions*** (such as to adjourn or recess), but I do not record them.
+
+## Main
+
+## Amend
+Class = S
+
+## Postpone to a Time Certain
+
+## Postpone Indefinitely
+
+## Close Debate
+
+## Previous Question
+
+## Table
+
+## Waive 231(c)
+
+## Take from Table
+
+## Reconsider
+
+## Recommit
+
+## Discharge
+
+# Schema for Motions
+
+## uid
+Unique identifier
+
+## council_period
+The council period (e.g., "20")
+
+## date
+the date of the meeting at which the motion was offered (e.g., "2014-01-07")
+
+## meeting
+the type of meeting (almost always "legislative meeting")
+
+## motion_number
+
+## measure_type
+
+## measure_number 
+
+## short_title
+## movant	
+## motion_description	
+## required_threshold	
+## description	
+## discussion
+## friendly
+## roll_call
+## (members' votes)
+A member's vote may be recorded as one of the following:
+
+* "Y" -- Yes (aka "Aye" or "Yea")
+* "N" -- No (aka "Nay")
+* "P" -- Present (aka "abstaining") 
+* "A" -- Absent
+* "R" -- Recused
+
+## passed
+
+# License
+Public domain
